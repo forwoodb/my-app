2b3e66ed962d1dbf3b50a9b13be6cd84b75d0f49
Brent Forwood <forwoodb77@gmail.com>
2019-09-20 22:10:10 -0400

    Updates

---
+++ precache-manifest.c62a6bbe3bca957a7d9e25bca2d13d22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc91c6132ac26782191106340e9eb629",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e82e69e152ea864ba6e8",
+    "url": "/my-app/static/css/main.c9a754f9.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/my-app/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "e82e69e152ea864ba6e8",
+    "url": "/my-app/static/js/main.1d0c6ae4.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
